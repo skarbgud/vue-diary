778b64b7f8378799b0ce4bd7e89b85b9147b3933
Nam <sjarbgud@gmail.com>
2020-07-19 20:20:30 +0900

    second commit

---
+++ 5.RockScissorsPaper/webpack.config.js
@@ -0,0 +1,34 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
+module.exports = {  //노드에 모듈을 만들었다
+    mode:'development',
+    devtool:'eval',
+    resolve:{
+        extensions:['.js','.vue']
+    },
+    entry:{
+        app: path.join(__dirname,'main.js'),
+    },
+    module:{
+        rules:[{
+            test:/\.vue$/,
+            loader: 'vue-loader',
+        },
+        {
+            test:/\.css$/,
+            use:[
+                'vue-style-loader',
+                'css-loader',
+            ] 
+        }]
+    },
+    plugins:[
+        new VueLoaderPlugin()
+    ],
+    output:{
+        filename: '[name].js',
+        path: path.join(__dirname,'dist'),
+        publicPath:'/dist',
+    }
+};
